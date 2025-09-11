73c013a254888738d0a4ffe90d6e90ccb4259949
Charles <cthorn@emich.edu>
2025-09-11 14:17:05 -0400

    Some good changes

--- apps/web/app/sign-out/route.js
+++ apps/web/app/sign-out/route.js
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 import { supabaseRoute } from '@/lib/supabase/route';
 
-export async function POST(request: Request) {
+export async function POST(request) {
   const supabase = supabaseRoute();
   await supabase.auth.signOut();
   return NextResponse.redirect(new URL('/', request.url));
